b0915d5acdd91ce1006c96de8f15bc65bb3aa656
SURIYA PRASATH S <suriyaprasad2002@hotmail.com>
2024-06-05 12:01:04 +0530

    SentenceReversese file is updated

--- src/stringPrgs/SentenceReverse.java
+++ src/stringPrgs/SentenceReverse.java
@@ -8,7 +8,15 @@ public class SentenceReverse {
         Scanner read = new Scanner(System.in);
 
         String original = read.nextLine();
+        String[] words = original.split(" ");
 
+        StringBuilder reversedSentence = new StringBuilder();
 
+        for(int i = words.length-1; i >= 0; i--){
+            //System.out.print(words[i] + " ");
+            reversedSentence.append(words[i]);
+            reversedSentence.append(" ");
+        }
+        System.out.println(reversedSentence);
     }
 }
