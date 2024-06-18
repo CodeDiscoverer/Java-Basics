5160116b005fe5b89c22a022f97c4b17c5b3dd95
SURIYA PRASATH S <suriyaprasad2002@hotmail.com>
2024-06-18 14:10:20 +0530

    Code Optimised

--- src/Collections/KingSoldier.java
+++ src/Collections/KingSoldier.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class KingSoldier {
     public static void main(String[] args) {
+
         Scanner read = new Scanner(System.in);
 
         System.out.print("Enter a sentence: ");
