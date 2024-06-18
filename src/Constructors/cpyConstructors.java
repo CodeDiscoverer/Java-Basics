40bab6caf90dcfecb16589282c9edb233d552596
SURIYA PRASATH S <suriyaprasad2002@hotmail.com>
2024-06-18 19:45:10 +0530

    Added the Copy Constructors

--- src/Constructors/cpyConstructors.java
+++ src/Constructors/cpyConstructors.java
@@ -2,21 +2,29 @@ package Constructors;
 
 import java.util.Scanner;
 
-class Student1{
+class Student1 {
     int rollNo;
     String name;
     float percentage;
 
-    Student1(int rollNo, String name, float percentage){
-        this.name = name;
+    // Regular constructor
+    Student1(int rollNo, String name, float percentage) {
         this.rollNo = rollNo;
+        this.name = name;
         this.percentage = percentage;
     }
 
-    void printDetails(){
-        System.out.println("Your name is " +name);
-        System.out.println("Your roll number is " +rollNo);
-        System.out.println("Your Percentage is " +percentage);
+    // Copy constructor (corrected)
+    Student1(Student1 original) {
+        this.rollNo = original.rollNo;
+        this.name = original.name;
+        this.percentage = original.percentage;
+    }
+
+    void printDetails() {
+        System.out.println("Your name is " + name);
+        System.out.println("Your roll number is " + rollNo);
+        System.out.println("Your Percentage is " + percentage);
     }
 }
 
@@ -24,11 +32,22 @@ public class cpyConstructors {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
 
+        System.out.println("Enter name:");
         String name = read.nextLine();
+        System.out.println("Enter roll number:");
         int rollNo = read.nextInt();
+        System.out.println("Enter percentage:");
         float percentage = read.nextFloat();
 
-        Student1 s = new Student1(rollNo, name, percentage);
-        s.printDetails();
+        // Using regular constructor to create Student1 object
+        Student1 s1 = new Student1(rollNo, name, percentage);
+        System.out.println("\nDetails of student 1:");
+        s1.printDetails();
+
+        // Using copy constructor to create another Student1 object
+        // Creating a copy of s1
+        Student1 s2 = new Student1(s1);
+        System.out.println("\nDetails of copied student 2:");
+        s2.printDetails();
     }
 }
