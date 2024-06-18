ff2ae4461432374677bea690a7f6289e2ce2bae4
SURIYA PRASATH S <suriyaprasad2002@hotmail.com>
2024-06-18 17:30:12 +0530

    Updated the file

--- src/Collections/CloneArray.java
+++ src/Collections/CloneArray.java
@@ -1,22 +1,22 @@
 package Collections;
 
 import java.util.*;
-import java.lang.*;
 
 public class CloneArray {
     public static void main(String[] args) {
 
         Scanner read = new Scanner(System.in);
-
-        System.out.print("Elements to store in List: ");
-        int n = read.nextInt();
-
         ArrayList<String> list = new ArrayList<>();
-        List<String> cloneList1 = (ArrayList<String>) list.clone();
+
+        System.out.print("Elements to store in the List 'count' followed by the 'elements': ");
+        int n = read.nextInt();
 
         for(int i=0; i<n; i++){
             list.add(read.next());
         }
 
+        List<String> cloneList1 = (ArrayList<String>) list.clone();
+
+        System.out.println(Arrays.toString(cloneList1.toArray()));
     }
 }
