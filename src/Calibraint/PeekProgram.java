a56cc5c8dfbdcfa226ac39d2cb3d13ab0a43cf47
SURIYA PRASATH S <suriyaprasad2002@hotmail.com>
2024-07-20 00:30:29 +0530

    Problem 2

---
+++ src/Calibraint/PeekProgram.java
@@ -0,0 +1,35 @@
+package Calibraint;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class PeekProgram {
+
+    public static void peekElem(List<Integer> list){
+
+        int max = Collections.max(list);
+        int indexMax = list.indexOf(max);
+
+        if(max > list.get(indexMax-1) && max > list.get(indexMax+1)){
+            System.out.println(max);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        List<Integer> list = new ArrayList<>();
+
+        System.out.print("Enter Value of n: ");
+        int n = read.nextInt();
+        System.out.println("Enter " + n + " Values into Array List: ");
+        for (int i = 0; i < n; i++) {
+            list.add(read.nextInt());
+        }
+
+        peekElem(list);
+    }
+}
