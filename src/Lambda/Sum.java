412a65f1a7fcc185f122e641bc5f7b169c85d326
SURIYA PRASATH S <suriyaprasad2002@hotmail.com>
2024-07-30 09:10:14 +0530

    Updated

--- src/Lambda/Sum.java
+++ src/Lambda/Sum.java
@@ -1,2 +1,15 @@
 package Lambda;
 
+@FunctionalInterface
+interface SumOperation {
+    int sum(int a, int b);
+}
+
+public class Sum {
+    public static void main(String[] args) {
+
+        SumOperation s = (a, b) -> a + b;
+
+        System.out.println("The sum is: " + s.sum(10, 20));
+    }
+}
