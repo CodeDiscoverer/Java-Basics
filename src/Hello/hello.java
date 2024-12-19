6e4c194cd526f245e826341ab26bf71f27a95895
SURIYA PRASATH S <suriyaprasad2002@hotmail.com>
2024-12-20 00:15:26 +0530

    Done #2 Completed Based On The Requirement

--- src/Hello/hello.java
+++ src/Hello/hello.java
@@ -2,7 +2,9 @@ package Hello;
 
 public class hello {
     public static void main(String[] args) {
+
         String h = "Hello";
+
         System.out.printf("%s\n", h);
         System.out.print("Hello,");
         System.out.println("This is Suriya");
