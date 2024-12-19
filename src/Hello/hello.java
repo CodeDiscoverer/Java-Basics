0a7e45dafa614af03c6a8d15e7a6ad89f9ffcf2e
SURIYA PRASATH S <suriyaprasad2002@hotmail.com>
2024-12-20 00:03:21 +0530

    In Progress #1 Now Working Based On The Requirements

--- src/Hello/hello.java
+++ src/Hello/hello.java
@@ -2,6 +2,8 @@ package Hello;
 
 public class hello {
     public static void main(String[] args) {
+        String h = "Hello";
+        System.out.printf("%s\n", h);
         System.out.print("Hello,");
         System.out.println("This is Suriya");
         System.out.println("How are you");
