51ab4ca31a92d2cc96e96456f0b0b8ff35b72d01
SURIYA PRASATH S <suriyaprasad2002@hotmail.com>
2024-12-20 09:45:59 +0530

    closed #4 - Demo Trial Resolved Succesfully

--- src/Hello/hello.java
+++ src/Hello/hello.java
@@ -4,6 +4,7 @@ public class hello {
     public static void main(String[] args) {
 
         String h = "Hello";
+
         System.out.printf("%s\n", h);
         System.out.print("Hello,");
         System.out.println("This is Suriya");
